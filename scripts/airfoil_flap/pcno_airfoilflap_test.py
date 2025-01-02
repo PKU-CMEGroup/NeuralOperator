90eaad277e27059a418bb130f48150b9504e16e6
JiayiZhou <ilevak22@qq.com>
2025-01-02 15:23:54 +0800

    Using argparse for improved parameter handling

--- scripts/airfoil_flap/pcno_airfoilflap_test.py
+++ scripts/airfoil_flap/pcno_airfoilflap_test.py
@@ -2,6 +2,8 @@ import torch
 import sys
 import os
 import numpy as np
+import argparse
+from datetime import datetime
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../..')))
@@ -56,7 +58,23 @@ try:
         sys.argv) > 1 else False
 except IndexError:
     PREPROCESS_DATA = False
-
+    
+parser = argparse.ArgumentParser(description='Train model with different types.')
+parser.add_argument('--train_type', type=str, default='mixed', choices=['standard', 'flap', 'mixed'],
+                    help='Type of training (standard, flap, mixed)')
+parser.add_argument('--feature_type', type=str, default='mach', choices=['pressure', 'mach', 'both'],
+                    help='Type of training (pressure, mach, both)')
+parser.add_argument('--n_train', type=int, default=1000, choices=[500, 1000, 1500],
+                    help='training datasize (500,1000,1500)')
+parser.add_argument('--train_sp_L', type=str, default='False', choices=['False' , 'together' , 'independently'],
+                    help='type of train_sp_L (False, together, independently )')
+parser.add_argument('--Lx', type=float, default=1, help='Initial value for Lx')
+parser.add_argument('--Ly', type=float, default=0.5, help='Initial value for Ly')
+parser.add_argument('--lr_ratio', type=float, default=10, help='lr ratio for independent training for L')
+args = parser.parse_args()
+current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+save_model_name = f"PCNO_airfoilplap_{args.train_type}_{args.feature_type}_n{args.n_train}_Lx{args.Lx}_Ly{args.Ly}_{current_time}"
+print(save_model_name, flush=True)
 ###################################
 # load data
 ###################################
@@ -107,12 +125,26 @@ edge_gradient_weights = torch.from_numpy(
 nodes_input = nodes.clone()
 
 ndata = nodes_input.shape[0]
-n_train, n_test = 1000, 500
+ndata1 = 1931
+ndata2 = 1932
+assert (
+    ndata == ndata1 + ndata2
+), "Expected ndata = ndata1 + ndata2. Ensure no data is omitted, or adjust the train and test index"
+n_train, n_test = args.n_train, 500
 m_train, m_test = n_train // 2, n_test // 2
-train_index = torch.cat([torch.arange(m_train),
-                        torch.arange(ndata - m_train, ndata)], dim=0)
-test_index = torch.cat([torch.arange(m_train, m_train + m_test),
-                       torch.arange(ndata - m_train - m_test, ndata - m_train)], dim=0)
+
+train_type = args.train_type
+if train_type == "flap":
+    train_index = torch.arange(n_train)
+elif train_type  == "standard":
+    train_index = torch.arange(ndata - n_train, ndata) 
+elif train_type == "mixed":
+    train_index = torch.cat(
+        [torch.arange(m_train), torch.arange(ndata - m_train, ndata)], dim=0
+    ) 
+
+test_index = torch.cat([torch.arange(ndata1 - m_test, ndata1),
+                       torch.arange(ndata1, ndata1 + m_test)], dim=0)
 
 
 x_train, x_test = nodes_input[train_index, ...], nodes_input[test_index, ...]
@@ -120,7 +152,21 @@ aux_train = (node_mask[train_index, ...], nodes[train_index, ...], node_weights[
              directed_edges[train_index, ...], edge_gradient_weights[train_index, ...])
 aux_test = (node_mask[test_index, ...], nodes[test_index, ...], node_weights[test_index, ...],
             directed_edges[test_index, ...], edge_gradient_weights[test_index, ...])
-y_train, y_test = features[train_index, :, [1]], features[test_index, :, [1]]
+
+feature_type = args.feature_type
+if feature_type == "mach":
+    feature_type_index = 1
+    out_dim=1
+elif feature_type == "pressure":
+    feature_type_index = 0
+    out_dim=1
+elif feature_type == "both":
+    feature_type_index = (0, 1)
+    out_dim=2
+y_train, y_test = (
+    features[train_index, ...][...,feature_type_index],
+    features[test_index, ...][...,feature_type_index],
+)
 
 
 ###################################
@@ -128,16 +174,17 @@ y_train, y_test = features[train_index, :, [1]], features[test_index, :, [1]]
 ###################################
 kx_max, ky_max = 32, 16
 ndim = 2
-train_sp_L = 'independently'
-
-Lx, Ly = 1.25, 0.5
+if args.train_sp_L == 'False':
+    args.train_sp_L = False
+train_sp_L = args.train_sp_L
+Lx, Ly = args.Lx, args.Ly
 print("Lx, Ly = ", Lx, Ly)
 modes = compute_Fourier_modes(ndim, [kx_max, ky_max], [Lx, Ly])
 modes = torch.tensor(modes, dtype=torch.float).to(device)
 model = PCNO(ndim, modes, nmeasures=1,
              layers=[128, 128, 128, 128, 128],
              fc_dim=128,
-             in_dim=2, out_dim=1,
+             in_dim=2, out_dim=out_dim,
              train_sp_L=train_sp_L,
              act='gelu').to(device)
 
@@ -146,10 +193,10 @@ base_lr = 0.001
 scheduler = "OneCycleLR"
 weight_decay = 1.0e-4
 batch_size = 20
-lr_ratio = 10
+lr_ratio = args.lr_ratio
 
-normalization_x = False
-normalization_y = False
+normalization_x = True
+normalization_y = True
 normalization_dim_x = []
 normalization_dim_y = []
 non_normalized_dim_x = 0
@@ -163,5 +210,5 @@ config = {"train": {"base_lr": base_lr, 'lr_ratio': lr_ratio, "weight_decay": we
           }
 
 train_rel_l2_losses, test_rel_l2_losses, test_l2_losses = PCNO_train(
-    x_train, aux_train, y_train, x_test, aux_test, y_test, config, model, save_model_name="./PCNO_airfoilflap_model"
+    x_train, aux_train, y_train, x_test, aux_test, y_test, config, model, save_model_name=save_model_name
 )

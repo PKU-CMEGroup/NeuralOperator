983ff39290314bc8ed66513f73e775be4444b76d
JiayiZhou <ilevak22@qq.com>
2025-01-10 16:00:13 +0800

    add rho

--- scripts/airfoil_flap/pcno_airfoilflap_test.py
+++ scripts/airfoil_flap/pcno_airfoilflap_test.py
@@ -5,11 +5,14 @@ import numpy as np
 import argparse
 from datetime import datetime
 
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 
-from pcno.geo_utility import preprocess_data, compute_node_weights
+from pcno.geo_utility import (
+    preprocess_data,
+    convert_structured_data,
+    compute_node_weights,
+)
 from pcno.pcno import compute_Fourier_modes, PCNO, PCNO_train
 
 
@@ -18,94 +21,53 @@ torch.set_printoptions(precision=16)
 torch.manual_seed(0)
 np.random.seed(0)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def load_data(data_path):
-
-    path1 = os.path.join(data_path, "Airfoil_flap_data/fluid_mesh")
-    path2 = os.path.join(data_path, "Airfoil_data/fluid_mesh")
-
-    ndata1 = 1932
-    ndata2 = 1931
-    elem_dim = 2
-
-    nodes_list = []
-    elems_list = []
-    features_list = []
-
-    for i in range(ndata1):
-        nodes_list.append(np.load(path1 + "/nodes_%05d" % (i) + ".npy"))
-        elems = np.load(path1 + "/elems_%05d" % (i) + ".npy")
-        elems_list.append(np.concatenate(
-            (np.full((elems.shape[0], 1), elem_dim, dtype=int), elems), axis=1))
-        features_list.append(np.load(path1 + "/features_%05d" % (i) + ".npy"))
-
-    for i in range(ndata2):
-        nodes_list.append(np.load(path2 + "/nodes_%05d" % (i) + ".npy"))
-
-        elems = np.load(path2 + "/elems_%05d" % (i) + ".npy")
-        elems_list.append(np.concatenate(
-            (np.full((elems.shape[0], 1), elem_dim, dtype=int), elems), axis=1))
-
-        features_list.append(np.load(path2 + "/features_%05d" % (i) + ".npy"))
-
-    return nodes_list, elems_list, features_list
-
-
-try:
-    PREPROCESS_DATA = sys.argv[1] == "preprocess_data" if len(
-        sys.argv) > 1 else False
-except IndexError:
-    PREPROCESS_DATA = False
-    
+PREPROCESS_DATA = False
 parser = argparse.ArgumentParser(description='Train model with different types.')
 parser.add_argument('--train_type', type=str, default='mixed', choices=['standard', 'flap', 'mixed'],
                     help='Type of training (standard, flap, mixed)')
-parser.add_argument('--feature_type', type=str, default='mach', choices=['pressure', 'mach', 'both'],
+parser.add_argument('--feature_type', type=str, default='pressure', choices=['pressure', 'mach', 'both'],
                     help='Type of training (pressure, mach, both)')
-parser.add_argument('--n_train', type=int, default=1000, choices=[500, 1000, 1500],
+parser.add_argument('--n_train', type=int, default=1000,
                     help='training datasize (500,1000,1500)')
-parser.add_argument('--train_sp_L', type=str, default='False', choices=['False' , 'together' , 'independently'],
+parser.add_argument('--train_sp_L', type=str, default='independently', choices=['False' , 'together' , 'independently'],
                     help='type of train_sp_L (False, together, independently )')
 parser.add_argument('--Lx', type=float, default=1, help='Initial value for Lx')
 parser.add_argument('--Ly', type=float, default=0.5, help='Initial value for Ly')
-parser.add_argument('--lr_ratio', type=float, default=10, help='lr ratio for independent training for L')
+parser.add_argument('--lr_ratio', type=float, default=10, help='lr_ratio')
+parser.add_argument('--rho', type=str, default='True', help='should add rho')
+
+
 args = parser.parse_args()
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-save_model_name = f"PCNO_airfoilplap_{args.train_type}_{args.feature_type}_n{args.n_train}_Lx{args.Lx}_Ly{args.Ly}_{current_time}"
+save_model_name = f"PCNO_airfoilplap_{args.train_type}_{args.feature_type}_n{args.n_train}_Lx{args.Lx}_Ly{args.Ly}_rho_{args.rho}_{current_time}"
 print(save_model_name, flush=True)
+
+
 ###################################
 # load data
 ###################################
 data_path = "../../data/airfoil_flap/"
-if PREPROCESS_DATA:
-    print("Loading data")
 
-    nodes_list, elems_list, feats_list = load_data(data_path)
-    nnodes, node_mask, nodes, node_measures_raw, features, directed_edges, edge_gradient_weights = preprocess_data(
-        nodes_list, elems_list, feats_list)
-    node_measures, node_weights = compute_node_weights(
-        nnodes, node_measures_raw, equal_measure=False)
-    node_equal_measures, node_equal_weights = compute_node_weights(
-        nnodes, node_measures_raw, equal_measure=True)
-    np.savez_compressed(data_path + "pcno_triangle_data.npz",
-                        nnodes=nnodes, node_mask=node_mask, nodes=nodes,
-                        node_measures=node_measures, node_weights=node_weights,
-                        node_equal_measures=node_equal_measures, node_equal_weights=node_equal_weights,
-                        features=features,
-                        directed_edges=directed_edges, edge_gradient_weights=edge_gradient_weights)
-    exit()
-else:
-    # load data
-    print("Loading data")
-    equal_weights = True
+print("Loading data")
+ndata1 = 1931
+ndata2 = 1932
+equal_weights = True
 
-    data = np.load(data_path + "pcno_triangle_data.npz")
-    nnodes, node_mask, nodes = data["nnodes"], data["node_mask"], data["nodes"]
-    node_weights = data["node_equal_weights"] if equal_weights else data["node_weights"]
-    directed_edges, edge_gradient_weights = data["directed_edges"], data["edge_gradient_weights"]
-    features = data["features"]
+data = np.load(data_path + "pcno_triangle_data.npz")
+nnodes, node_mask, nodes = data["nnodes"], data["node_mask"], data["nodes"]
+
+node_equal_weights = data["node_equal_weights"] 
+node_measures = data["node_measures"]
+node_measures_raw = data["node_measures_raw"]
+indices = np.isfinite(node_measures_raw)
+node_rhos = np.copy(node_equal_weights)
+node_rhos[indices] = node_rhos[indices]/node_measures[indices]
+
+directed_edges, edge_gradient_weights = data["directed_edges"], data["edge_gradient_weights"]
+features = data["features"]
 
 
 ###################################
@@ -115,21 +77,23 @@ print("Casting to tensor")
 nnodes = torch.from_numpy(nnodes)
 node_mask = torch.from_numpy(node_mask)
 nodes = torch.from_numpy(nodes.astype(np.float32))
-node_weights = torch.from_numpy(node_weights.astype(np.float32))
+node_equal_weights = torch.from_numpy(node_equal_weights.astype(np.float32))
+node_rhos = torch.from_numpy(node_rhos.astype(np.float32))
 features = torch.from_numpy(features.astype(np.float32))
 directed_edges = torch.from_numpy(directed_edges)
-edge_gradient_weights = torch.from_numpy(
-    edge_gradient_weights.astype(np.float32))
+edge_gradient_weights = torch.from_numpy(edge_gradient_weights.astype(np.float32))
 
 # This is important
 nodes_input = nodes.clone()
+in_dim = 2
+if args.rho == "True":
+    nodes_input = torch.cat([nodes_input, node_rhos], dim=-1)
+    in_dim = 3
 
 ndata = nodes_input.shape[0]
-ndata1 = 1931
-ndata2 = 1932
-assert (
-    ndata == ndata1 + ndata2
-), "Expected ndata = ndata1 + ndata2. Ensure no data is omitted, or adjust the train and test index"
+
+print(ndata)
+
 n_train, n_test = args.n_train, 500
 m_train, m_test = n_train // 2, n_test // 2
 
@@ -143,16 +107,27 @@ elif train_type == "mixed":
         [torch.arange(m_train), torch.arange(ndata - m_train, ndata)], dim=0
     ) 
 
-test_index = torch.cat([torch.arange(ndata1 - m_test, ndata1),
-                       torch.arange(ndata1, ndata1 + m_test)], dim=0)
+test_index = torch.cat(
+    [torch.arange(ndata1 - m_test, ndata1), torch.arange(ndata1, ndata1 + m_test)],
+    dim=0,
+)
 
 
 x_train, x_test = nodes_input[train_index, ...], nodes_input[test_index, ...]
-aux_train = (node_mask[train_index, ...], nodes[train_index, ...], node_weights[train_index, ...],
-             directed_edges[train_index, ...], edge_gradient_weights[train_index, ...])
-aux_test = (node_mask[test_index, ...], nodes[test_index, ...], node_weights[test_index, ...],
-            directed_edges[test_index, ...], edge_gradient_weights[test_index, ...])
-
+aux_train = (
+    node_mask[train_index, ...],
+    nodes[train_index, ...],
+    node_equal_weights[train_index, ...],
+    directed_edges[train_index, ...],
+    edge_gradient_weights[train_index, ...],
+)
+aux_test = (
+    node_mask[test_index, ...],
+    nodes[test_index, ...],
+    node_equal_weights[test_index, ...],
+    directed_edges[test_index, ...],
+    edge_gradient_weights[test_index, ...],
+)
 feature_type = args.feature_type
 if feature_type == "mach":
     feature_type_index = 1
@@ -167,26 +142,37 @@ y_train, y_test = (
     features[train_index, ...][...,feature_type_index],
     features[test_index, ...][...,feature_type_index],
 )
-
+print(
+    f"x train:{x_train.shape}, y train:{y_train.shape}, x test:{x_test.shape}, y test:{y_test.shape}",
+    flush=True,
+)
 
 ###################################
 # train
 ###################################
 kx_max, ky_max = 32, 16
 ndim = 2
+
 if args.train_sp_L == 'False':
     args.train_sp_L = False
 train_sp_L = args.train_sp_L
+
 Lx, Ly = args.Lx, args.Ly
 print("Lx, Ly = ", Lx, Ly)
+
 modes = compute_Fourier_modes(ndim, [kx_max, ky_max], [Lx, Ly])
 modes = torch.tensor(modes, dtype=torch.float).to(device)
-model = PCNO(ndim, modes, nmeasures=1,
-             layers=[128, 128, 128, 128, 128],
-             fc_dim=128,
-             in_dim=2, out_dim=out_dim,
-             train_sp_L=train_sp_L,
-             act='gelu').to(device)
+model = PCNO(
+    ndim,
+    modes,
+    nmeasures=1,
+    layers=[128, 128, 128, 128, 128],
+    fc_dim=128,
+    in_dim=in_dim,
+    out_dim=out_dim,
+    train_sp_L=train_sp_L,
+    act="gelu",
+).to(device)
 
 epochs = 500
 base_lr = 0.001
@@ -203,12 +189,78 @@ non_normalized_dim_x = 0
 non_normalized_dim_y = 0
 
 
-config = {"train": {"base_lr": base_lr, 'lr_ratio': lr_ratio, "weight_decay": weight_decay, "epochs": epochs, "scheduler": scheduler, "batch_size": batch_size,
-                    "normalization_x": normalization_x, "normalization_y": normalization_y,
-                    "normalization_dim_x": normalization_dim_x, "normalization_dim_y": normalization_dim_y,
-                    "non_normalized_dim_x": non_normalized_dim_x, "non_normalized_dim_y": non_normalized_dim_y}
-          }
+config = {
+    "train": {
+        "base_lr": base_lr,
+        "lr_ratio": lr_ratio,
+        "weight_decay": weight_decay,
+        "epochs": epochs,
+        "scheduler": scheduler,
+        "batch_size": batch_size,
+        "normalization_x": normalization_x,
+        "normalization_y": normalization_y,
+        "normalization_dim_x": normalization_dim_x,
+        "normalization_dim_y": normalization_dim_y,
+        "non_normalized_dim_x": non_normalized_dim_x,
+        "non_normalized_dim_y": non_normalized_dim_y,
+    }
+}
+
 
 train_rel_l2_losses, test_rel_l2_losses, test_l2_losses = PCNO_train(
-    x_train, aux_train, y_train, x_test, aux_test, y_test, config, model, save_model_name=save_model_name
+    x_train,
+    aux_train,
+    y_train,
+    x_test,
+    aux_test,
+    y_test,
+    config,
+    model,
+    save_model_name=save_model_name,
 )
+
+# print("Generating Figure Data", flush=True)
+# # model.train()
+# with torch.no_grad():
+
+#     n_train = 10
+#     n_test = 10
+#     m_train, m_test = n_train // 2, n_test // 2
+
+#     train_index = torch.cat(
+#         [torch.arange(m_train), torch.arange(ndata - m_train, ndata)], dim=0
+#     ) 
+#     test_index = torch.cat([torch.arange(ndata1 - m_test, ndata1),
+#                         torch.arange(ndata1, ndata1 + m_test)], dim=0)
+
+
+#     x_train, x_test = nodes_input[train_index, ...], nodes_input[test_index, ...]
+#     aux_train = (node_mask[train_index, ...], nodes[train_index, ...], node_equal_weights[train_index, ...],
+#                 directed_edges[train_index, ...], edge_gradient_weights[train_index, ...])
+#     aux_test = (node_mask[test_index, ...], nodes[test_index, ...], node_equal_weights[test_index, ...],
+#                 directed_edges[test_index, ...], edge_gradient_weights[test_index, ...])
+#     y_train, y_test = features[train_index, :, :], features[test_index, :, :]
+#     print(f"x train:{x_train.shape}, y train:{y_train.shape}, x test:{x_test.shape}, y test:{y_test.shape}", flush=True)
+
+
+#     node_mask_train, nodes_train, node_weights_train, directed_edges_train, edge_gradient_weights_train = aux_train
+#     x_train, node_mask_train, nodes_train, node_weights_train, directed_edges_train, edge_gradient_weights_train = x_train.to(device), node_mask_train.to(device), nodes_train.to(device), node_weights_train.to(device), directed_edges_train.to(device), edge_gradient_weights_train.to(device)
+#     out_train = model(x_train, (node_mask_train, nodes_train, node_weights_train, directed_edges_train, edge_gradient_weights_train))
+#     out_train=out_train * node_mask_train
+
+#     node_mask_test, nodes_test, node_weights_test, directed_edges_test, edge_gradient_weights_test = aux_test
+#     x_test, node_mask_test, nodes_test, node_weights_test, directed_edges_test, edge_gradient_weights_test = x_test.to(device), node_mask_test.to(device), nodes_test.to(device), node_weights_test.to(device), directed_edges_test.to(device), edge_gradient_weights_test.to(device)
+#     out_test = model(x_test, (node_mask_test, nodes_test, node_weights_test, directed_edges_test, edge_gradient_weights_test))
+#     out_test=out_test * node_mask_test
+
+
+#     np.savez_compressed("./figs/" + save_model_name + "_fig.npz", 
+#                         x_train=x_train.cpu().numpy(), 
+#                         y_train=y_train.cpu().numpy(),
+#                         out_train=out_train.cpu().numpy(), 
+#                         node_mask_train=node_mask_train.cpu().numpy(),
+#                         x_test=x_test.cpu().numpy(), 
+#                         y_test=y_test.cpu().numpy(),
+#                         out_test=out_test.cpu().numpy(),
+#                         node_mask_test=node_mask_test.cpu().numpy(),
+#                         )

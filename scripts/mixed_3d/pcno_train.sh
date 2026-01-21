f961d203a6119e328973199bff44e886e3cd91bb
Daniel-Zhengyu-Huang <hzyhzy2014@gmail.com>
2026-01-22 07:57:13 +0800

    fix typo in exterior_laplacian bash file

--- scripts/mixed_3d/pcno_train.sh
+++ scripts/mixed_3d/pcno_train.sh
@@ -4,7 +4,7 @@
 #SBATCH -J PCNO_train_vertex_centered
 #SBATCH -p GPU80G
 #SBATCH --nodes=1 
-#SBATCH --ntasks=32
+#SBATCH --ntasks=16
 #SBATCH --gres=gpu:1
 #SBATCH --time=100:00:00
 
@@ -17,7 +17,7 @@ python pcno_geo_mixed_3d_train.py   --grad True \
                                     --batch_size 5 \
                                     --epochs 500 \
                                     --n_train 1000 \
-                                    --n_test 100 \
+                                    --n_test 500 \
                                     --to_divide_factor 1.0 \
                                     --mesh_type "vertex_centered" \
                                     > logs/PCNO_mixed_3d_grad_geo_vertex_centered.log

4b8322fae9a2c8d3d931dc0f846e1bc7427e5495
Daniel-Zhengyu-Huang <hzyhzy2014@gmail.com>
2026-03-02 14:54:45 +0800

    reset some files

--- scripts/curve/mpcno_train.sh
+++ scripts/curve/mpcno_train.sh
@@ -9,30 +9,30 @@
 #SBATCH --time=100:00:00
 
 # ========== params ==========
-KERNEL_TYPE="exterior_laplace_neumann"
+KERNEL_TYPE="dp_laplace"
 
 GRAD="True"
 GEO="True"
 GEOINTEGRAL="True"
 
-N_TRAIN=8000
+N_TRAIN=2000
 N_TEST=1000
 N_TWO_CIRCLES_TEST=1000
 
 TO_DIVIDE_FACTOR=20.0
-BATCH_SIZE=32
+BATCH_SIZE=8
 LAYERS=(64 64 64 64 64 64)
 ACT="gelu"
 GEO_ACT='softsign'
 
-K_MAX=64
+K_MAX=32
 # =============================
 
 
 LAYER_SIZES_STR=$(IFS=,; echo "${LAYERS[*]}")
 LOG_DIR="log/1_1_5_2d_${KERNEL_TYPE}/${LAYER_SIZES_STR}_${ACT}/"
 mkdir -p ${LOG_DIR}
-module load conda
+
 source activate pytorch 
 
 python mpcno_curve_test.py \

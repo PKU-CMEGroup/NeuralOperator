5372942fc188665ed5ee81396c12244d36140252
xhhwyh <wyhan@stu.pku.edu.cn>
2026-06-02 18:46:35 +0800

    Add PCNO multi-distribution training

--- pcno/pcno.py
+++ pcno/pcno.py
@@ -1,4 +1,5 @@
 import math
+import os
 import numpy as np
 import torch
 import sys
@@ -611,7 +612,163 @@ class Combinedscheduler_OneCycleLR:
             # Load the state of the second scheduler
             self.scheduler2.load_state_dict(state_dict['scheduler2'])
 
-        
+
+
+
+
+
+# x_train, y_train, x_test, y_test are [n_data, n_x, n_channel] arrays
+def PCNO_train_multidist(x_train, aux_train, y_train, x_test_list, aux_test_list, y_test_list, config, model, label_test_list=None, save_model_name="./PCNO_model", checkpoint_path=None):
+    assert len(x_test_list) == len(y_test_list) == len(aux_test_list), "The length of x_test_list, y_test_list and aux_test_list should be the same"
+    n_distributions = len(x_test_list)
+    n_train = x_train.shape[0]
+    train_rel_l2_losses = []
+    test_rel_l2_losses = []
+    test_l2_losses = []
+    normalization_x, normalization_y = config["train"]["normalization_x"], config["train"]["normalization_y"]
+    normalization_dim_x, normalization_dim_y = config["train"]["normalization_dim_x"], config["train"]["normalization_dim_y"]
+    non_normalized_dim_x, non_normalized_dim_y = config["train"]["non_normalized_dim_x"], config["train"]["non_normalized_dim_y"]
+
+    ndims = model.ndims # n_train, size, n_channel
+    print("In PCNO_train, ndims = ", ndims)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    if normalization_x:
+        x_normalizer = UnitGaussianNormalizer(x_train, non_normalized_dim=non_normalized_dim_x, normalization_dim=normalization_dim_x)
+        x_train = x_normalizer.encode(x_train)
+        for i in range(n_distributions):
+            x_test_list[i] = x_normalizer.encode(x_test_list[i])
+        x_normalizer.to(device)
+
+    if normalization_y:
+        y_normalizer = UnitGaussianNormalizer(y_train, non_normalized_dim=non_normalized_dim_y, normalization_dim=normalization_dim_y)
+        y_train = y_normalizer.encode(y_train)
+        for i in range(n_distributions):
+            y_test_list[i] = y_normalizer.encode(y_test_list[i])
+        y_normalizer.to(device)
+
+    node_mask_train, nodes_train, node_weights_train, directed_edges_train, edge_gradient_weights_train = aux_train
+    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train, node_mask_train, nodes_train, node_weights_train, directed_edges_train, edge_gradient_weights_train),
+                                               batch_size=config['train']['batch_size'], shuffle=True)
+
+    test_loaders = []
+    for i in range(n_distributions):
+        node_mask_test, nodes_test, node_weights_test, directed_edges_test, edge_gradient_weights_test = aux_test_list[i]
+        sub_dataset = torch.utils.data.TensorDataset(
+            x_test_list[i],
+            y_test_list[i],
+            node_mask_test,
+            nodes_test,
+            node_weights_test,
+            directed_edges_test,
+            edge_gradient_weights_test,
+        )
+        sub_loader = torch.utils.data.DataLoader(sub_dataset, batch_size=config['train']['batch_size'], shuffle=False)
+        if label_test_list is not None:
+            name = label_test_list[i]
+        else:
+            name = f"Distribution_{i}"
+        test_loaders.append((name, sub_loader))
+
+    myloss = LpLoss(d=1, p=2, size_average=False)
+
+    optimizer = CombinedOptimizer(model.normal_params, model.inv_L_scale_params,
+        betas=(0.9, 0.999),
+        lr=config["train"]["base_lr"],
+        lr_ratio=config["train"]["lr_ratio"],
+        weight_decay=config["train"]["weight_decay"],
+        )
+
+    scheduler = Combinedscheduler_OneCycleLR(
+        optimizer, max_lr=config['train']['base_lr'], lr_ratio=config["train"]["lr_ratio"],
+        div_factor=2, final_div_factor=100, pct_start=0.2,
+        steps_per_epoch=1, epochs=config['train']['epochs'])
+
+    current_epoch, epochs = 0, config['train']['epochs']
+
+    if checkpoint_path:
+        checkpoint = torch.load(checkpoint_path, weights_only=True)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        current_epoch = checkpoint['current_epoch'] + 1
+        print("resetart from epoch : ", current_epoch)
+
+    for ep in range(current_epoch, epochs):
+        t1 = default_timer()
+        train_rel_l2 = 0
+
+        model.train()
+        for x, y, node_mask, nodes, node_weights, directed_edges, edge_gradient_weights in train_loader:
+            x, y, node_mask, nodes, node_weights, directed_edges, edge_gradient_weights = x.to(device), y.to(device), node_mask.to(device), nodes.to(device), node_weights.to(device), directed_edges.to(device), edge_gradient_weights.to(device)
+
+            batch_size_ = x.shape[0]
+            optimizer.zero_grad()
+            out = model(x, (node_mask, nodes, node_weights, directed_edges, edge_gradient_weights))
+            if normalization_y:
+                out = y_normalizer.decode(out)
+                y = y_normalizer.decode(y)
+            out = out * node_mask
+            loss = myloss(out.view(batch_size_, -1), y.view(batch_size_, -1))
+            loss.backward()
+
+            optimizer.step()
+            train_rel_l2 += loss.item()
+
+        test_rel_l2_dict = {}
+        test_l2_dict = {}
+
+        model.eval()
+        with torch.no_grad():
+            for name, loader in test_loaders:
+                test_l2 = 0
+                test_rel_l2 = 0
+                for x, y, node_mask, nodes, node_weights, directed_edges, edge_gradient_weights in loader:
+                    x, y, node_mask, nodes, node_weights, directed_edges, edge_gradient_weights = x.to(device), y.to(device), node_mask.to(device), nodes.to(device), node_weights.to(device), directed_edges.to(device), edge_gradient_weights.to(device)
+
+                    batch_size_ = x.shape[0]
+                    out = model(x, (node_mask, nodes, node_weights, directed_edges, edge_gradient_weights))
+
+                    if normalization_y:
+                        out = y_normalizer.decode(out)
+                        y = y_normalizer.decode(y)
+                    out = out * node_mask
+                    test_rel_l2 += myloss(out.view(batch_size_, -1), y.view(batch_size_, -1)).item()
+                    test_l2 += myloss.abs(out.view(batch_size_, -1), y.view(batch_size_, -1)).item()
+                test_l2 /= len(loader.dataset)
+                test_rel_l2 /= len(loader.dataset)
+                test_rel_l2_dict[name] = test_rel_l2
+                test_l2_dict[name] = test_l2
+
+        scheduler.step()
+
+        train_rel_l2 /= n_train
+        train_rel_l2_losses.append(train_rel_l2)
+        test_rel_l2_losses.append(test_rel_l2_dict)
+        test_l2_losses.append(test_l2_dict)
+
+        t2 = default_timer()
+        print("Epoch : ", ep, " Time: ", round(t2-t1,3), " Rel. Train L2 Loss : ", train_rel_l2, " Rel. Test L2 Loss : ", test_rel_l2_dict, " Test L2 Loss : ", test_l2_dict,
+              " inv_L_scale: ", [round(float(x[0]), 3) for x in (scaled_sigmoid(model.inv_L_scale_latent, model.inv_L_scale_min, model.inv_L_scale_max)).cpu().tolist()],
+              flush=True)
+        if ((ep % 100 == 99) or (ep == epochs - 1)) and save_model_name:
+            torch.save(model.state_dict(), save_model_name + ".pth")
+
+            if os.environ.get("PCNO_DISABLE_RECOVERY_CHECKPOINT") != "1":
+                torch.save({
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'scheduler_state_dict': scheduler.state_dict(),
+                    'current_epoch': ep,
+                }, "checkpoint.pth")
+
+    return train_rel_l2_losses, test_rel_l2_losses, test_l2_losses
+
+
+
+
+
 
 
 
@@ -740,23 +897,17 @@ def PCNO_train(x_train, aux_train, y_train, x_test, aux_test, y_test, config, mo
         if ((ep %100 == 99) or (ep == epochs -1)) and save_model_name:    
             torch.save(model.state_dict(), save_model_name + ".pth")
 
-            torch.save({
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict(),
-                'current_epoch': ep,  # optional: to track training progress
-            }, "checkpoint.pth")
+            if os.environ.get("PCNO_DISABLE_RECOVERY_CHECKPOINT") != "1":
+                torch.save({
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'scheduler_state_dict': scheduler.state_dict(),
+                    'current_epoch': ep,  # optional: to track training progress
+                }, "checkpoint.pth")
 
             
     
     
     return train_rel_l2_losses, test_rel_l2_losses, test_l2_losses
-
-
-
-
-
-
-
 
 
